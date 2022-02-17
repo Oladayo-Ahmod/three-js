80cd79c083c80c6c5a4838979622384a8a7833cd
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-17 01:27:58 +0100

    add a plane

--- threejs/main.js
+++ threejs/main.js
@@ -2,8 +2,13 @@ import * as THREE from 'three'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight,0.1,1000)
 const renderer = new THREE.WebGLRenderer()
-console.log(camera)
-console.log(renderer)
 // to use the renderer
 renderer.setSize(innerWidth,innerHeight) // set the size of the renderer
 document.body.appendChild(renderer.domElement)
+// create a plane
+const Boxgeometry = new THREE.BoxGeometry(1,1,1) // box geometry
+const material = new THREE.MeshBasicMaterial({color:  0xffffff}) // mesh material
+const mesh = new THREE.Mesh(Boxgeometry,material) // mesh
+scene.add(mesh)
+camera.position.z = 5
+renderer.render(scene,camera) // render 
