16acb1a3e68f8b5beaa00e58c42d2ee8c547279e
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-17 16:40:01 +0100

    implementing gui on the plane geometry

--- threejs/main.js
+++ threejs/main.js
@@ -4,12 +4,28 @@ import * as dat from 'dat.gui'
 const  gui = new dat.GUI()
 const world  = {
     plane : {
-        width : 10
+        width : 10,
+        height : 10
     }
 }
+// width gui
 gui.add(world.plane,'width',1,20).onChange(()=>{
     planeMesh.geometry.dispose()
-    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width,10,10,10)
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width,world.plane.height,10,10)
+
+    const {array} = planeMesh.geometry.attributes.position;
+    for (let i = 0; i < array.length; i += 3) {
+        const x = array[i];
+        const y = array[i + 1];
+        const z = array[i + 2];
+        array[i +2] = z + Math.random()
+        
+    }
+})
+// height gui
+gui.add(world.plane,'height',1,20).onChange(()=>{
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width,world.plane.height,10,10)
 
     const {array} = planeMesh.geometry.attributes.position;
     for (let i = 0; i < array.length; i += 3) {
