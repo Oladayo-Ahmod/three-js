fb66659d2e04a8491e8f9f79140b80f735cf106a
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-17 01:38:46 +0100

    animating the box geometry

--- threejs/main.js
+++ threejs/main.js
@@ -4,6 +4,7 @@ const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight,0.1,1000)
 const renderer = new THREE.WebGLRenderer()
 // to use the renderer
 renderer.setSize(innerWidth,innerHeight) // set the size of the renderer
+renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 // create a plane
 const Boxgeometry = new THREE.BoxGeometry(1,1,1) // box geometry
@@ -11,4 +12,11 @@ const material = new THREE.MeshBasicMaterial({color:  0xffffff}) // mesh materia
 const mesh = new THREE.Mesh(Boxgeometry,material) // mesh
 scene.add(mesh)
 camera.position.z = 5
-renderer.render(scene,camera) // render 
+// create animate function
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera) // render 
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.01
+}
+animate()
