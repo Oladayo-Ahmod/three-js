179db89f691c63d5bff47dfe23f472fff472986a
oladayo ahmod <oladayoahmod112@gmail.com>
2022-03-01 18:17:32 +0100

    modify the design

--- threejs/script.js
+++ threejs/script.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js'
 
 var typed = new Typed('.skills', {
-    strings: ["First sentence.", "Second sentence."],
+    strings: ["Front-end Developer.", "Back-end Developer.","Full Stack Developer."],
     typeSpeed: 40,
     loop: true,
     shuffle: true
